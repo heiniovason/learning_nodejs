d058650b7a522664124db8b0d342ff6a1445ac41
heiniovason <heini.ovason@gmail.com>
2019-06-02 18:59:31 +0000

    Notes app initialized

---
+++ importing_core_modules/notes-app/app.js
@@ -0,0 +1,6 @@
+const getNotes = require('./notes.js')
+console.log(getNotes())
+
+// const fs = require('fs')
+// fs.writeFileSync('notes.txt', 'Adding some text!\n')
+//fs.appendFileSync('notes.txt', 'Appending some text.\n')
