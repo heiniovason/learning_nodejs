71ce7573d7a104d4738dc9b930850b09d7397512
heiniovason <heini.ovason@gmail.com>
2019-06-02 20:13:49 +0000

    Fooling around with colors in console prints

--- import_modules/notes-app/app.js
+++ import_modules/notes-app/app.js
@@ -1,6 +1,9 @@
-const getNotes = require('./notes.js')
-console.log(getNotes())
+const chalk = require('chalk');
 
+console.log(chalk.white.bold.bgGreen('Success!'));
+
+//const getNotes = require('./notes.js')
+//console.log(getNotes())
 // const fs = require('fs')
 // fs.writeFileSync('notes.txt', 'Adding some text!\n')
 //fs.appendFileSync('notes.txt', 'Appending some text.\n')
