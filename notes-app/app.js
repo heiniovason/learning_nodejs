90c2ba3bea21d0d32b9c071657a0f10dcf3a2e7b
heiniovason <heini.ovason@gmail.com>
2019-06-12 08:14:50 +0000

    commands and handelrs added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,40 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-console.log(chalk.white.bold.bgGreen('Success!'));
+// Customise app version with yargs. Default value is '1.0.0'.
+//yargs.version('1.0.1')
 
-//const getNotes = require('./notes.js')
-//console.log(getNotes())
-// const fs = require('fs')
-// fs.writeFileSync('notes.txt', 'Adding some text!\n')
-//fs.appendFileSync('notes.txt', 'Appending some text.\n')
+yargs.command({
+    command: 'add',
+    describe: 'Adding note',
+    handler: function() {
+        console.log(chalk.green('Adding note!'))
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removing note',
+    handler: function() {
+        console.log(chalk.red('Removing note!'))
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Reads note',
+    handler: function() {
+        console.log(chalk.green('Reading note!'))
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'Lists notes',
+    handler: function() {
+        console.log(chalk.green('Listing files!'))
+    }
+})
+
+console.log(yargs.argv)
